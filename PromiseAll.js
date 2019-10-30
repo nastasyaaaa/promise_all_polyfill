4ae62e2c18643d794a2853c4a9d122e3948e8df2
Nana <nastyaa1212@gmail.com>
2019-10-30 08:31:28 +0200

    redone promise.all

--- PromiseAll.js
+++ PromiseAll.js
@@ -2,29 +2,34 @@ function customPromiseAll(promises)
 {
 	return new Promise(function(resolve, reject) {
 
+		if(promises.length === 0) {
+			resolve(0);
+		}
+
 		let resolved = 0;
 
 		let isResolved = () => {
+			resolved++;
 			if(resolved === promises.length) {
 				resolve(resolved);
 			}
 		};
 
 		for(let i = 0; i < promises.length; i++) {
-			promises[i].finally(() => {
-				resolved++;  
-				isResolved();
-		 	});
+			promises[i].then(isResolved, reject);
 		}
 
 	});
 }
 
 customPromiseAll([
-		new Promise((resolve, rej) => { resolve('1'); }).then((result) => {alert(result + ' alert');}),
-		new Promise((resolve, rej) => { setTimeout(() => {resolve('2 ')}, 1000) }).then((result) => {alert(result + ' alert');}),
-		new Promise((resolve, rej) => { resolve('3'); }).then((result) => {alert(result + ' alert');}),
-		new Promise((resolve, rej) => { setTimeout(() => {resolve('4 ')}, 2000) }).then((result) => {alert(result + ' alert');})
+		new Promise((resolve, rej) => { resolve('1'); }),
+		new Promise((resolve, rej) => { setTimeout(() => {rej('2 ')}, 1000) }),
+		new Promise((resolve, rej) => { resolve('3'); }),
+		new Promise((resolve, rej) => { setTimeout(() => {resolve('4 ')}, 2000) })
 
-]).then((resolved) => {alert(resolved + ' promises resolved!')});
+	]).then(
+		(resolved) => {console.log(resolved + ' promises resolved!')},
+		(rejected) => {console.log(rejected + "promise rejected!")}
+	);
 
